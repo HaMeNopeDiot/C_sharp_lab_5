1b712137bca1b83b081bea6d339bab1e3669d31b
staruhin_dm <staruhin_dm@C1R012N07>
2022-11-17 12:48:09 +0300

    Добавьте файлы проекта.

---
+++ C_sharp_lab_5/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C_sharp_lab_5
+{
+    public partial class Form1 : Form
+    {
+        int status = -1;
+        string name_file = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        // сохранение файла
+        void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox1.Text);
+            MessageBox.Show("Файл сохранен");
+            button7.Enabled = true;
+        }
+        // открытие файла
+        void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox1.Text = fileText;
+            MessageBox.Show("Файл открыт");
+            name_file = filename;
+            button7.Enabled = true;
+            return;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String_text_changer Test = new String_text_changer();
+            string result = "";
+            if (status == 1)
+                result = Test.give_result(textBox1.Text, textBox3.Text);
+            if (status == 2)
+                result = Test.give_result_non_string(textBox1.Text, textBox3.Text);
+            textBox2.Text = result;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+            button4.Enabled = false;
+            textBox2.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox2.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (status == -1)
+            {
+                button3.Enabled = true;
+                makeResultToolStripMenuItem.Enabled = true;
+            }
+            status = 2;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (status == -1)
+            {
+                button3.Enabled = true;
+                makeResultToolStripMenuItem.Enabled = true;
+            }
+            status = 1;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            name_file = "unnamed";
+            button7.Enabled = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(name_file, textBox1.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e) // open
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox1.Text = fileText;
+            MessageBox.Show("Файл открыт");
+            name_file = filename;
+            button7.Enabled = true;
+            saveFileAsToolStripMenuItem.Enabled = true;
+            return;
+        }
+
+        private void newFileToolStripMenuItem_Click(object sender, EventArgs e) // new
+        {
+            textBox1.Text = "";
+            name_file = "";
+            button7.Enabled = false;
+
+            //saveFileAsToolStripMenuItem.Enabled = false;
+
+            makeResultToolStripMenuItem.Enabled = true;
+            clearResultToolStripMenuItem.Enabled = false;
+            saveResultToolStripMenuItem.Enabled = false;
+
+            textBox2.Clear();
+        }
+
+        private void saveFileAsToolStripMenuItem_Click(object sender, EventArgs e) // save
+        {
+            if(name_file != "")
+            {
+                System.IO.File.WriteAllText(name_file, textBox1.Text);
+                MessageBox.Show("Файл сохранен " + name_file);
+            }
+            else
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                // получаем выбранный файл
+                string filename = saveFileDialog1.FileName;
+                // сохраняем текст в файл
+                System.IO.File.WriteAllText(filename, textBox1.Text);
+                MessageBox.Show("Файл сохранен " + name_file);
+                button7.Enabled = true;
+                //saveFileAsToolStripMenuItem.Enabled = true;
+                name_file = filename;
+            }
+
+        }
+
+        private void saveFileAsToolStripMenuItem1_Click(object sender, EventArgs e) // save as
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox1.Text);
+            MessageBox.Show("Файл сохранен " + name_file);
+            button7.Enabled = true;
+            //saveFileAsToolStripMenuItem.Enabled = true;
+            name_file = filename;
+        }
+
+        private void makeResultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String_text_changer Test = new String_text_changer();
+            string result = "";
+            if (status == 1)
+                result = Test.give_result(textBox1.Text, textBox3.Text);
+            if (status == 2)
+                result = Test.give_result_non_string(textBox1.Text, textBox3.Text);
+            textBox2.Text = result;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            //saveResultToolStripMenuItem.Enabled = true;
+            clearResultToolStripMenuItem.Enabled = true;
+        }
+
+        private void saveResultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox2.Text);
+            MessageBox.Show("Файл сохранен");
+            button7.Enabled = true;
+            //saveFileAsToolStripMenuItem.Enabled = true;
+            name_file = filename;
+        }
+    }
+}
---
+++ C_sharp_lab_5/String_text_changer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_sharp_lab_5
+{
+    public class String_text_changer
+    {
+
+		public String_text_changer()
+		{
+		}
+
+		public string give_result(string file_text, string word)
+        {
+            string file_result = "";
+            if(!check_is_word(word))
+            {
+                return file_result;
+            }
+            else
+            {
+                string[] strokes = file_text.Split(new char[] { '\n' });
+                for(int i = 0; i < strokes.Length; i++)
+                {
+                    if(strokes[i].Contains(word))
+                    {
+                        int start = strokes[i].IndexOf(word);
+                        //(start != 0 && strokes[i][start - 1] == ' ') || (start + word.Length + 1 < strokes[i].Length && strokes[i][start + word.Length + 1] == ' ')
+                        bool flag = true;
+                        if (start > 0 && strokes[i][start - 1] != ' ')
+                            flag = false;
+                        if (strokes[i].Length > start + word.Length + 1 && (strokes[i][start + word.Length + 1] != ' '))
+                            flag = false;
+                        if(flag)
+                            file_result += strokes[i] + '\n';
+                    }
+                }
+                return file_result;
+            }
+            
+
+        }
+
+        public string give_result_non_string(string file_text, string word)
+        {
+            string file_result = "";
+            if (!check_is_word(word))
+            {
+                return file_result;
+            }
+            else
+            {
+                bool flag = false;
+                string text_stroke = "";
+                for(int i =0;i <= file_text.Length;i++)
+                {
+                    if(i == file_text.Length || file_text[i] == '\n')
+                    {
+                        flag = is_word_in_stroke(text_stroke, word);
+                        if(flag)
+                        {
+                            file_result += text_stroke + "\n";
+                        }
+                        flag = false;
+                        text_stroke = "";
+                    }
+                    else
+                    {
+                        text_stroke += file_text[i];
+                    }
+                }
+
+
+
+                return file_result;
+            }
+        }
+
+        public bool is_word_in_stroke(string stroke, string word)
+        {
+            for(int i = 0; i <= stroke.Length - word.Length; i++)
+            {
+                bool cond = true;
+                int j;
+                for (j = 0; j<word.Length && cond;j++)
+                {
+                    if (word[j] != stroke[i + j])
+                        cond = false;
+                }
+                if(cond && stroke.Length > i + j + 1 && stroke[i + j + 1] != ' ')
+                    cond = false;
+                if (cond && i > 0 && (stroke[i - 1] != ' '))
+                    cond = false;
+
+                if (cond)
+                    return true;
+
+            }
+            return false;
+        }
+
+		public bool check_is_word(string word)
+        {
+            int i = 0;
+            while(word.Length > i)
+            {
+                if (!is_symbol(word[i]))
+                    return false;
+                i++;
+            }
+            return true;
+        }
+
+        public bool is_symbol(char symbol)
+        {
+            if (symbol == ' ' || symbol == '\n' || symbol == '\t')
+                return false;
+            else
+                return true;
+        }
+    }
+}
